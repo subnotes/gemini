bc2760075de513339ee5ca66a2c3780f09dca2d9
Jonathan Chang <jonathan.chang512@gmail.com>
2017-05-11 04:22:44 +0000

    Some folder organization, and first pass at flashcard explorer.

--- app/src/FlashcardExplorer.js
+++ app/src/FlashcardExplorer.js
@@ -1,12 +1,37 @@
 // import node packages
 import React from 'react';
 
-function FlashcardExplorer(props) {
-  return (
-    <div>
-      <h3>Flashcard Explorer</h3>
-    </div>
-  )
-}
+// import local components
+import FlashcardContainer from './containers/FlashcardContainer';
+
+// Local flashcard array for testing
+
+// Component for Explorer View
+var FlashcardExplorer = React.createClass({
+	getInitialState: function () {
+		return { };
+	},
+
+	render: function () {
+		return (
+			<div>
+			  <h3>Flashcard Explorer</h3>
+			    <div>
+			      <ol>
+			        <FlashcardContainer 
+				  question="question 1"
+				  answer="answer 1" />
+			        <FlashcardContainer 
+				  question="question 2"
+				  answer="answer 2" />
+			        <FlashcardContainer 
+				  question="question 3"
+				  answer="answer 3" />
+			      </ol>
+			    </div>
+			</div>
+		);
+	}
+});
 
 export default FlashcardExplorer;
