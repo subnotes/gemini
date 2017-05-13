ff017da5caf227e5d686d08c895eff303fc35896
Jordan White <emailwhitej@gmail.com>
2017-05-13 09:12:46 -0700

    rearanging

---
+++ app/src/config/driveConfigs.js
@@ -0,0 +1,8 @@
+export function getDriveConfigs() {
+  const configs = {
+    discoverDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"], 
+    clientId: '38703598827-318fe1f76ju71nedjkudad6gcvteglii.apps.googleusercontent.com', 
+    scope: 'https://www.googleapis.com/auth/drive'
+  }
+  return configs
+}
