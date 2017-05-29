cf39baf87850ce4ab3a6e2c2f428d97b9363d3af
Jordan White <emailwhitej@gmail.com>
2017-05-28 20:21:01 -0700

    converted Dashboard to class

--- app/src/Dashboard.js
+++ app/src/Dashboard.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-function Dashboard(props) {
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    if (typeof this.props.library !== 'undefined') {
+      return(
+        <div>
+          <h3>Dashboard</h3>
+          <h4>Your Notebooks</h4>
+          <ul>
+            {Object.entries(this.props.library).map(item => (
+              <li key={item[0]}><Link to={"/notebook/" + item[0]}>{item[1]['fileName']}</Link></li>
+            ))}
+          </ul>
+        </div>
+      )
+    }
+    else {
+      return (<div>no subnotes in this notebook</div>)
+    }
+  }
 
-  if (typeof props.library !== 'undefined') {
-    return(
-      <div>
-        <h3>Dashboard</h3>
-        <h4>Your Notebooks</h4>
-        <ul>
-          {Object.entries(props.library).map(item => (
-            <li key={item[0]}><Link to={"/notebook/" + item[0]}>{item[1]['fileName']}</Link></li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
-  else {
-    return (<div>no subnotes in this notebook</div>)
-  }
 }
 
 export default Dashboard;
