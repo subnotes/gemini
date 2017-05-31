a6aac92c0a2d697e2ebbda10f58c9f7d1735654e
Jordan White <emailwhitej@gmail.com>
2017-05-30 17:51:10 -0700

    Dashboard now creating and uploading empty notbook object

--- app/src/Dashboard.js
+++ app/src/Dashboard.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {createNotebook, uploadNotebook} from './helpers/googleDrive'
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      newName: ""
+    }
+    this.newNotebook = this.newNotebook.bind(this)
+    this.setNewName = this.setNewName.bind(this)
+  }
+  
+  newNotebook(e) {
+    //createNotebook(e.target.value)
+    createNotebook(this.state.newName + '.sn', (response) => {uploadNotebook({}, response.result.id, this.props.loadNotebooks)})
+    e.preventDefault()
+    this.setState({newName: ""})
+  }
+  
+  setNewName(e){
+    this.setState({"newName": e.target.value})
   }
   
   render() {
@@ -17,6 +34,15 @@ class Dashboard extends Component {
               <li key={item[0]}><Link to={"/notebook/" + item[0]}>{item[1]['fileName']}</Link></li>
             ))}
           </ul>
+          <h4>New Notebook</h4>
+          <form onSubmit={this.newNotebook}>
+            <div>            
+              <input name="newName" value={this.state.newName} type="text" onChange={this.setNewName}/>.sn
+            </div>
+            <div>
+              <input type="submit" value="Create New Notebook" />
+            </div>
+          </form>
         </div>
       )
     }
